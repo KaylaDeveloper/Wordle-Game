d8529942c2bb65bba801e1e6804bb3f1f386fcbf
Kayla Lee <kaylalee2326@gmail.com>
2023-02-07 10:42:21 +1100

    first commit

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import Guess from "../Guess";
+import GuessBoard from "../GuessBoard";
+import { checkGuess } from "../../game-helpers";
+import Banner from "../Banner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +13,24 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guessResult, setGuessResult] = React.useState([]);
+  const [value, setValue] = React.useState("");
+
+  const handleSubmitGuess = (guess) => {
+    const checkedGuess = checkGuess(guess, answer);
+    setGuessResult([...guessResult, checkedGuess]);
+    setValue(guess);
+  };
+  return (
+    <>
+      <GuessBoard guessResult={guessResult} />
+      {value === answer || guessResult.length === 6 ? (
+        <Banner value={value} answer={answer} guessResult={guessResult} />
+      ) : (
+        <Guess handleSubmitGuess={handleSubmitGuess} />
+      )}
+    </>
+  );
 }
 
 export default Game;
